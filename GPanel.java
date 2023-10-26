e82b1681ad91610fc6be9fed939e7640cfc994ec
KapcinskiMaciej <147518550+KapcinskiMaciej@users.noreply.github.com>
2023-10-26 16:05:09 +0200

    Add files via upload

--- GPanel.java
+++ GPanel.java
@@ -2,11 +2,11 @@ package gra;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+
 
 public class GPanel extends JPanel{
     int x = 100;
-    Button btnstart, btncheck;
+    Button btnstart;
     JProgressBar hp;
     Image background_img;
     Image rat_img;
@@ -16,32 +16,29 @@ public class GPanel extends JPanel{
 
     GPanel (int Frame_x, int Frame_y){
         this.setSize(Frame_x, Frame_y);
+        background_img = new ImageIcon("src\\gra\\graphics\\background.png").getImage();
+        rat_img = new ImageIcon("src\\gra\\graphics\\rat.png").getImage();
         Interface(Frame_x,Frame_y);
 
     }
-    @Override
-    public void paintComponent(Graphics g){
-        Graphics2D gr2D=(Graphics2D)g;
-        gr2D.drawImage(background_img, 0,0,this.getWidth(),this.getHeight(),null);
-        gr2D.drawImage(rat_img,423,310,null);
-    }
-    private void Interface(int f_x, int f_y){
+
+    public void Interface(int f_x, int f_y){
         //definicje skladowych interfejsu
         setLayout(new BorderLayout());
         btnstart = new Button("START");
-        btncheck = new Button("CHECK");
         JTextField txtdown = new JTextField("Tutaj wpisz slowo",1);
-        TextArea txtup = new TextArea("Wyswietlane slowo");
+        JTextField txtup = new JTextField("Wyswietlane slowo");
         hp = new JProgressBar(0,100);
         Font myfont = new Font("Open Sans",Font.BOLD,20);
 
         txtdown.setFont(myfont);
         txtup.setEditable(false);
+        txtup.setFont(myfont);
         hp.setValue(x);
 
-        Labels(btnstart,btncheck,txtdown,txtup,hp,f_x);
+        Labels(btnstart,txtdown,txtup,hp,f_x);
     }
-    private void Labels(Button btnstart, Button btncheck, JTextField txtdown, TextArea txtup, JProgressBar hp,int f_x){
+    public void Labels(Button btnstart, JTextField txtdown, JTextField txtup, JProgressBar hp,int f_x){
         JLabel labeldown = new JLabel();
         JLabel labelup = new JLabel();
         //donly label
@@ -49,7 +46,6 @@ public class GPanel extends JPanel{
         labeldown.setLayout(new GridLayout(1,3));
         labeldown.add(btnstart);
         labeldown.add(txtdown);
-        labeldown.add(btncheck);
         labeldown.setPreferredSize(new Dimension(f_x,40));
         //gorny label
         labelup.setBackground(Color.black);
@@ -60,5 +56,13 @@ public class GPanel extends JPanel{
 
         add(labeldown, BorderLayout.SOUTH);
         add(labelup, BorderLayout.NORTH);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        Graphics2D gr2D=(Graphics2D)g;
+
+        gr2D.drawImage(background_img, 0,0,this.getWidth(),this.getHeight(),null);
+        gr2D.drawImage(rat_img,423,310,null);
     }
 }
